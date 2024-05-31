bf3eeb6fd0d0d8b84754c7322348f14c15a2182c
mahsarahima <91888752+MahsaRahima@users.noreply.github.com>
2024-05-31 19:31:36 +0700

    Update lowongan.blade.php

--- resources/views/lowongan.blade.php
+++ resources/views/lowongan.blade.php
@@ -2,10 +2,10 @@
     <x-card>        
         <div class="flex justify-between"> 
             <button class="bg-laravel text-white rounded py-5 px-10 hover:bg-white ml-10"> 
-                <a href="/loker" class="text-black">Lowongan Kerja</a> 
+                <a href="{{ route('listings.index') }}" class="text-black">Lowongan Kerja</a> 
             </button> 
             <button class="bg-laravel text-white rounded py-5 px-10 hover:bg-white mr-10"> 
-                <a href="/logang" class="text-black">Lowongan Magang</a> 
+                <a href="{{ route('listingmagang.index') }}" class="text-black">Lowongan Magang</a> 
             </button> 
         </div>
     </x-card>
